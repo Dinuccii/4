3ba10ec31b7671c2b9eb85220bdfcdf4b372f031
Dinuccii <105287898+Dinuccii@users.noreply.github.com>
2022-05-31 08:45:15 -0300

    Add files via upload

---
+++ calculadora/js java.js
@@ -0,0 +1,49 @@
+var btn = document.querySelectorAll(".key li")
+imput = document.querySelector(".input-valor")
+operador = document.querySelectorAll(".operador")
+
+for(var i = 0; i<btn.length; i++){
+    document.onkeypress = function(event){
+        var key = event.charCode;
+        //mudança para navegador
+        //firefox interpretar a calculadora
+for(var e = 0; e <= 10;){
+    if(key === (48+e)){
+        input.innerHTML += e;}
+}}
+switch (key){ 
+    case l1:
+        input.innerHTML +="*";
+        break;
+        case l2:
+        input.innerHTML +="*";
+        break; 
+        case 17:
+        input.innerHTML +="-";
+        break; 
+        case 18:
+        input.innerHTML +=".";
+        break; 
+        case 19:
+        input.innerHTML +="/";
+        case 20:
+        case 70: 
+        var equacao = input.innerHTML = eval(equacao);
+        catch (e) {
+        alert('Error na expressao') 
+        break; 
+        case 55:
+        case 77:
+        input.innerHTML ="";
+        break; 
+        default:
+        break;
+        };
+        bnt[i].addEventListener ('click',function() {
+        var btnval = this.innerHeight,
+        inputval = input.innerHeight;
+        //console.log(btnval);
+        })}
+        
+    
+        
